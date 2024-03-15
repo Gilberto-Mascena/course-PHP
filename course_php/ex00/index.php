bf8fa230271cf88806f2d3ec37f9021da442497f
Gilberto Mascena <gilbertomascena@gmail.com>
2024-03-14 21:45:25 -0300

    added project

---
+++ course_php/ex00/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primrira pagina em PHP</title>
+</head>
+<body>
+    
+    <h1>
+        <?php
+            echo "Olá, mundo! \u{1F30E}";
+        ?>
+    </h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At illo ullam vitae accusamus consectetur asperiores distinctio tempore id consequatur. Odit, dignissimos error fugit voluptatem sit quod. Ipsam iste ullam autem!</p>
+
+</body>
+</html>
