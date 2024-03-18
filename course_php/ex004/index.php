11a4bc6c2f6018acdf99c4d9701415e2e6b8dd83
Gilberto Mascena <gilbertomascena@gmail.com>
2024-03-18 20:10:32 -0300

    added new files

---
+++ course_php/ex004/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pr-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+
+<body>
+
+    <section class="container">
+
+        <h1 class="title">Tipos primitivos escalares em PHP!</h1>
+
+        <div class="content">
+
+            <?php
+
+            // Em PHP, os tipos primitivos escalares são: int, float, (Real, até a 7.4), string e bool.
+            // Em PHP, 0x=hexadecimal, 0=octal e 0b=binary.
+
+            $numero1 = 300;
+            echo "<p class=\"paragraph\">O valor da variável 1 é = <span class=\"value\">$numero1</span><br></p>";
+
+            // Hexadecimal
+            $numero2 = 0x1A;
+            echo "<p class=\"paragraph\">O valor da variável 2 é = <span class=\"value\">$numero2</span><br></p>";
+
+            // Binário
+            $numero3 = 0b1101;
+            echo "<p class=\"paragraph\">O valor da variável 3 é = <span class=\"value\">$numero3</span><br></p>";
+
+            // Octal
+            $numero4 = 0123;
+            echo "<p class=\"paragraph\">O valor da variável 4 é = <span class=\"value\">$numero4</span><br></p>";
+
+           // $variavel = 3.14;
+           //var_dump() é uma função que retorna o tipo e o valor da variável.
+           // var_dump($variavel);
+
+            ?>
+
+        </div>
+
+    </section>
+
+</body>
+
+</html>
